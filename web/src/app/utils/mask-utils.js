8327cf0cbf9abaebe5b89bcc74124c317af846d8
cassius.avila <cassius.avila@edu.pucrs.br>
2021-03-29 11:36:59 -0300

    :sparkles: adição de inputs que estavam faltando na criação de uma denúncia

--- web/src/app/utils/mask-utils.js
+++ web/src/app/utils/mask-utils.js
@@ -1,10 +1,11 @@
 class MaskUtils {
   cpfMask(cpf) {
-    cpf = cpf.replace(/\D/g, "");
-    cpf = cpf.replace(/^(\d{3})/g, "$1.");
-    cpf = cpf.replace(/(\d{3}).(\d{3})/g, "$1.$2.");
-    cpf = cpf.replace(/(\d{3}).(\d{3}).(\d{3})/g, "$1.$2.$3-");
-    return cpf;
+    return cpf
+      .replace(/\D/g, "")
+      .replace(/(\d{3})(\d)/, "$1.$2")
+      .replace(/(\d{3})(\d)/, "$1.$2")
+      .replace(/(\d{3})(\d{1,2})/, "$1-$2")
+      .replace(/(-\d{2})\d+?$/, "$1");
   }
 
   cnpjMask(cnpj) {
@@ -15,6 +16,14 @@ class MaskUtils {
       .replace(/(\d{3})(\d)/, "$1/$2")
       .replace(/(\d{4})(\d)/, "$1-$2")
       .replace(/(-\d{2})\d+?$/, "$1");
+  }
+
+  phoneMask(phone) {
+    return phone
+      .replace(/\D/g, "")
+      .replace(/(\d{2})(\d)/, "($1) $2")
+      .replace(/(\d{4,5})(\d)/, "$1-$2")
+      .replace(/(-\d{4})\d+?$/, "$1");
   }
 }
 
