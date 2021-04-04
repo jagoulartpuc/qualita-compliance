0be64278421358858884465057bbd1af118c131b
cassius.avila <cassius.avila@edu.pucrs.br>
2021-04-03 21:30:28 -0300

    :sparkles: inclusão de chamada para criação de denúncia

---
+++ web/src/services/report.service.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const reportInstance = axios.create({
+  baseURL: process.env.REACT_APP_API_BASE_URL,
+});
+
+export function createReport(payload) {
+  const newPayload = {
+    category: payload.category,
+    urgent: payload.urgent,
+    description: payload.description,
+    author: payload.user.name,
+    cpf: payload.user.cpf,
+    email: payload.user.email,
+    phone: payload.user.phone,
+    dates: [payload.date],
+    envolvedPeople: payload.recipentsEmails,
+  };
+  return reportInstance.post("denuncia", newPayload);
+}
