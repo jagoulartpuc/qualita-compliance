635a65a60578d9da240f8b17453942d9f1bbbf96
Rodrigo Domingos Machado <rodrigodomingosmachado@MacBook-Air-de-Rodrigo.local>
2021-07-13 20:01:55 -0300

    feat: Add go back button.

--- web/src/app/pages/trainning-details/index.jsx
+++ web/src/app/pages/trainning-details/index.jsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from "react";
-import { useHistory } from "react-router";
 import { getTrainning, repplyComment, addNewComment } from "@Services";
 import "./style.scss";
 import GenericPdfImage from '@Images/generic-pdf.png';
 import GenericImage from '@Images/generic-image.png';
 import { Component } from "react";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 function TrainningItem({ value }) {
   return (
@@ -144,7 +144,13 @@ export default class TrainningModuleDetailPage extends Component {
       <h1>Carregando...</h1>
     ) : (
       <section className='trainning-details'>
-        <main className=''>
+        <main>
+          <div className='goback'>
+            <a onClick={() => this.props.history.replace("/treinamentos")}>
+              <FontAwesomeIcon icon={faArrowLeft} />
+              <span>Voltar</span>
+            </a>
+          </div>
           <div className='trainnning-content'>
             <section id='trainning-title'>
               <TrainningItem value={this.state.trainning?.title} />
@@ -176,155 +182,3 @@ export default class TrainningModuleDetailPage extends Component {
     );
   }
 }
-
-
-
-
-
-// export default function TrainningModuleDetailPage({ match }) {
-//   const history = useHistory();
-
-
-//   const 
-
-
-
-//   const 
-
-//   return 
-// }
-
-
-
-
-// function TrainningItem({ value }) {
-//   return (
-//     <p>{value}</p>
-//   );
-// }
-
-// function NewComment({ display }) {
-//   return (
-//     <div className='new-comment'>
-//       <textarea ></textarea>
-//       <a>Enviar</a>
-//     </div>
-//   );
-// }
-
-// export default function TrainningModuleDetailPage({ match }) {
-//   const [trainning, setTrainning] = useState(null);
-//   const [loading, setLoading] = useState(false);
-//   const { id } = match.params;
-//   const [activeReply, setActiveReply] = useState([]);
-
-//   const history = useHistory();
-//   useEffect(() => {
-//     const fetchTrainning = async () => {
-//       try {
-//         setLoading(true);
-//         const { data: trainningResponse } = await getTrainning(id);
-//         setTrainning(trainningResponse);
-//         setLoading(false);
-//       } catch (error) {
-//         history.replace("/");
-//       }
-//     };
-
-//     fetchTrainning();
-//   }, []);
-//   const YoutubeEmbed = ({ videoLink }) => (
-//     <div className="video-responsive">
-//       <iframe
-//         height="480"
-//         src={videoLink}
-//         frameBorder="0"
-//         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-//         allowFullScreen
-//         title="Embedded youtube"
-//       />
-//     </div>
-//   );
-
-//   const parseYouTubeUrlToEmbedded = () => trainning?.videoLink?.replace('watch?v=', 'embed/');
-
-//   const getComments = () => {
-
-//     return Object.keys(trainning?.comments).map((key, index) => {
-//       const commentBase = trainning?.comments[key].find(comment => comment.id === key);
-//       const subComments = trainning?.comments[key].map(comment => {
-//         if (comment.id !== key) {
-//           return (
-//             <div key={index} className='comment-reply'>
-//               <p><strong>{comment.name}:</strong> {comment.comment}</p>
-//             </div>
-//           );
-//         }
-//       });
-
-//       return (
-//         <div className='comment'>
-//           <p><strong>{commentBase.name}:</strong> {commentBase.comment}</p>
-//           <p><a onClick={(e) => {
-//             e.preventDefault();
-//             activeReply[index] = true;
-//             setActiveReply(activeReply);
-//           }}>Responder</a></p>
-//           {activeReply[index] ? <NewComment /> : null}
-//           {subComments}
-//         </div>
-//       );
-//     });
-
-//   };
-
-
-//   const getAttachments = () => {
-//     return trainning?.attachments.map(attach => {
-//       const contentForDownload = `data:${attach.mimeType};base64,${attach.base64Adress}`;
-
-//       return (<div className='attachment'>
-//         <span>{attach.name}</span>
-//         <br />
-//         <a download={attach.name} href={contentForDownload}>
-//           <img className='attachment-icon' src={attach.mimeType === 'application/pdf' ? GenericPdfImage : GenericImage} />
-//         </a>
-//       </div>);
-//     });
-//   }
-
-//   return loading || !trainning ? (
-//     <h1>Carregando...</h1>
-//   ) : (
-//     <section className='trainning-details'>
-//       <main className=''>
-//         <div className='trainnning-content'>
-//           <section id='trainning-title'>
-//             <TrainningItem value={trainning?.title} />
-//           </section>
-//           <br />
-//           <section>
-//             <YoutubeEmbed videoLink={parseYouTubeUrlToEmbedded()} />
-//           </section>
-
-//           <br />
-//           <section id='trainning-description'>
-//             <TrainningItem value={trainning?.description} />
-//           </section>
-//           <br />
-//           <section id='trainning-comments'>
-//             <p><strong>Comentários</strong></p>
-//             {getComments()}
-//             <NewComment diplay={true} />
-//           </section>
-//           <br />
-//         </div>
-
-//       </main>
-//       <aside id='trainning-material'>
-//         <h3><strong>Materiais do treinamento</strong></h3>
-//         {getAttachments()}
-//       </aside>
-//     </section>
-//   );
-// }
