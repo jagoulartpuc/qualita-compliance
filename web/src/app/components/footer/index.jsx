edc1d446f828457e435c3744baf780ba13ad2567
jagoulartpuc <jose.goulart@acad.pucrs.br>
2021-07-19 17:32:58 -0300

    update cnpjs

--- web/src/app/components/footer/index.jsx
+++ web/src/app/components/footer/index.jsx
@@ -8,7 +8,7 @@ export function Footer() {
 
     return (
         <footer id="footer-component">
-            <p className="footer-text">© {year} Qualitá  Compliance. Cnpj 53.725.188/0001-00. Todos os diretos reservados</p>
+            <p className="footer-text">© {year} Qualitá  Compliance. Cnpj 19.765.660/0001-91. Todos os diretos reservados</p>
         </footer>
     )
 }
